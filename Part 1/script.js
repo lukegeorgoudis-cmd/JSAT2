1d15d6feee0dd9fca40d8d830446bd522a3f966b
lukegeorgoudis-cmd <lukegeorgoudis@gmail.com>
2025-12-11 19:35:33 +1100

    Mass Upload

---
+++ Part 1/script.js
@@ -0,0 +1,6 @@
+// Create an array with the given numbers
+let numbers = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
+
+// Print the array to the console
+console.log("Q1.1 - Original array:");
+console.log(numbers);
